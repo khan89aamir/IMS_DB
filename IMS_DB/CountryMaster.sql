611e1c99ad44fe84519b1cfda4125733841b9fd0
AAMIR <khan89aamir@gmail.com>
2020-02-05 03:09:13 +0530

    1.Added CountryMaster table script.

---
+++ IMS_DB/CountryMaster.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE [dbo].[CountryMaster](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[CountryCode] [nvarchar](10) NULL,
+	[CountryName] [nvarchar](50) NULL,
+	[ActiveStatus] [bit] NOT NULL CONSTRAINT [DF_CountryMaster_ActiveStatus]  DEFAULT ((1)),
+	[CreatedBy] [int] NOT NULL,
+	[CreatedOn] [datetime] NOT NULL CONSTRAINT [DF_CountryMaster_CreatedOn]  DEFAULT (getdate()),
+	[UpdatedBy] [int] NULL,
+	[UpdatedOn] [datetime] NULL,
+ CONSTRAINT [PK_CountryMaster] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
