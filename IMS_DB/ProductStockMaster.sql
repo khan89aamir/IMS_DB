ab3a1c5d29bf02b1e32f34e631b2cc3616403a21
AAMIR <khan89aamir@gmail.com>
2020-02-16 16:27:52 +0530

    1.added column called StoreID. 2.applied primary column ProductID and StoreID

--- IMS_DB/ProductStockMaster.sql
+++ IMS_DB/ProductStockMaster.sql
@@ -3,14 +3,12 @@ CREATE TABLE [dbo].[ProductStockMaster](
 	[ProductStockID] [int] IDENTITY(1,1) NOT NULL,
 	[ProductID] [int] NOT NULL,
 	[QTY] [int] NOT NULL,
+	[StoreID] [int] NOT NULL CONSTRAINT [DF_ProductStockMaster_StoreID]  DEFAULT ((0)),
 	[CreatedBy] [int] NOT NULL CONSTRAINT [DF_ProductStockMaster_CreatedBy]  DEFAULT ((0)),
 	[CreatedOn] [datetime] NOT NULL CONSTRAINT [DF_ProductStockMaster_CreatedOn]  DEFAULT (getdate()),
 	[UpdatedBy] [int] NULL,
-	[UpdatedOn] [datetime] NULL,
- CONSTRAINT [PK_ProductStockMaster] PRIMARY KEY CLUSTERED 
-(
-	[ProductStockID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[UpdatedOn] [datetime] NULL, 
+    CONSTRAINT [PK_ProductStockMaster] PRIMARY KEY ([ProductStockID],[ProductID],[StoreID])
 ) ON [PRIMARY]
 
 GO
