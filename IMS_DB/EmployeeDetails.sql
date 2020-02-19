dfd2778fbc39cc4dc1215e713a8528b2fee87977
AAMIR <khan89aamir@gmail.com>
2020-02-20 02:42:37 +0530

    1. Added Photo column in Product Master form. 2. Added Default datetime value for createdon column in EmployeeMaster form. 3. Changed datatype from datetime to date for DOB column in EmployeeMaster form.

--- IMS_DB/EmployeeDetails.sql
+++ IMS_DB/EmployeeDetails.sql
@@ -1,15 +1,20 @@
-﻿CREATE TABLE [dbo].[EmployeeDetails]
+﻿CREATE TABLE [dbo].[EmployeeDetails](
+	[EmpID] [int] IDENTITY(1,1) NOT NULL,
+	[EmployeeCode] [nvarchar](50) NULL,
+	[Name] [nvarchar](50) NULL,
+	[ShopID] [int] NULL,
+	[Gender] [bit] NULL,
+	[DOB] [date] NULL,
+	[Address] [nvarchar](max) NULL,
+	[Photo] [varbinary](max) NULL,
+	[CreatedBy] [int] NOT NULL CONSTRAINT [DF_EmployeeDetails_CreatedBy]  DEFAULT ((0)),
+	[CreatedOn] [datetime] NOT NULL CONSTRAINT [DF_EmployeeDetails_CreatedOn]  DEFAULT (getdate()),
+	[UpdatedBy] [int] NULL,
+	[UpdatedOn] [datetime] NULL,
+ CONSTRAINT [PK__Employee__AF2DBA7930B08991] PRIMARY KEY CLUSTERED 
 (
-	[EmpID] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [EmployeeCode] NVARCHAR(50) NULL, 
-    [Name] NVARCHAR(50) NULL, 
-    [ShopID] INT NULL, 
-    [Gender] BIT NULL, 
-    [DOB] DATETIME NULL, 
-    [Address] NVARCHAR(MAX) NULL, 
-    [Photo] VARBINARY(MAX) NULL, 
-    [CreatedBy] INT NULL, 
-    [CreatedOn] DATETIME NULL, 
-    [UpdatedBy] INT NULL, 
-    [UpdatedOn] DATETIME NULL
-)
+	[EmpID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
