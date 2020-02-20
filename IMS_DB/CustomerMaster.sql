887a5c46fb88138dc30cb54d9c0ee2788c324459
Ashfaque <Ashfaque@ASHFAQUE>
2020-02-20 15:40:30 +0300

    added new table CustomerMaster

---
+++ IMS_DB/CustomerMaster.sql
@@ -0,0 +1,19 @@
+CREATE TABLE [dbo].[CustomerMaster](
+	[CustomerID] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerName] [nvarchar](max) NULL,
+	[CustomerAddress] [nvarchar](max) NULL,
+	[PhoneNo] [nvarchar](50) NULL,
+	[ActiveStatus] [bit] NOT NULL,
+	[CreatedBy] [int] NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[UpdatedBy] [int] NULL,
+	[UpdatedOn] [datetime] NULL,
+ CONSTRAINT [PK_CustomerMaster] PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[CustomerMaster] ADD  CONSTRAINT [DF_CustomerMaster_CreatedOn]  DEFAULT (getdate()) FOR [CreatedOn]
+GO
