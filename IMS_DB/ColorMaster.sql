a0f5fdce2413dbb594957fe6bdb1e4d8b43401a4
AAMIR <khan89aamir@gmail.com>
2020-02-23 03:49:40 +0530

    1.Added SizeType Master table 2.Added Size Master table 3.Added color Master table 4.Added columns in Purchase Invoice table.

---
+++ IMS_DB/ColorMaster.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ColorMaster](
+	[ColorID] [int] IDENTITY(1,1) NOT NULL,
+	[ColorName] [nvarchar](50) NOT NULL,
+	[ActiveStatus] [bit] NOT NULL CONSTRAINT [DF_ColorMaster_ActiveStatus]  DEFAULT ((1)),
+	[CreatedBy] [int] NOT NULL CONSTRAINT [DF_ColorMaster_CreatedBy]  DEFAULT ((0)),
+	[CreatedOn] [datetime] NOT NULL CONSTRAINT [DF_ColorMaster_CreatedOn]  DEFAULT (getdate()),
+	[UpdatedBy] [int] NULL,
+	[UpdatedOn] [datetime] NULL,
+ CONSTRAINT [PK_ColorMaster] PRIMARY KEY CLUSTERED 
+(
+	[ColorID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
---
+++ IMS_DB/SizeMaster.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[SizeMaster](
+	[SizeID] [int] IDENTITY(1,1) NOT NULL,
+	[Size] [int] NULL,
+	[SizeTypeID] [int] NOT NULL,
+	[ActiveStatus] [bit] NOT NULL CONSTRAINT [DF_SizeMaster_ActiveStatus]  DEFAULT ((1)),
+	[CreatedBy] [int] NOT NULL CONSTRAINT [DF_SizeMaster_CreatedBy]  DEFAULT ((0)),
+	[CreatedOn] [datetime] NOT NULL CONSTRAINT [DF_SizeMaster_CreatedOn]  DEFAULT (getdate()),
+	[UpdatedBy] [int] NULL,
+	[UpdatedOn] [datetime] NULL,
+ CONSTRAINT [PK_SizeMaster] PRIMARY KEY CLUSTERED 
+(
+	[SizeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
