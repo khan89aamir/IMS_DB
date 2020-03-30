8226595252a72c6ce756aebe86f23aad2345fc1b
Abdul Mateen Khan <abdulmateen50@gmail.com>
2020-03-30 14:01:30 +0530

    DB Changes .. Lost view found and new barcode table

---
+++ IMS_DB/View_SalesDetails.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[View_SalesDetails]
+	AS 
+    SELECT        s1.InvoiceID, p1.ProductName, s1.QTY, s1.Rate, sm.Size, c1.ColorName
+FROM            dbo.SalesDetails AS s1 INNER JOIN
+                         dbo.ProductMaster AS p1 ON s1.ProductID = p1.ProductID INNER JOIN
+                         dbo.SizeMaster AS sm ON sm.SizeID = s1.SizeID INNER JOIN
+                         dbo.ColorMaster AS c1 ON c1.ColorID = s1.ColorID
---
+++ IMS_DB/tblBarCodeSettings.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[tblBarCodeSettings]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [BarCodeSetting] NVARCHAR(MAX) NULL
+)
