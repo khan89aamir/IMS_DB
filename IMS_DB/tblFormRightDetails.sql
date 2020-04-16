43228871df6ec4d3710f7042255e064c521e0552
Abdul Mateen Khan <abdulmateen50@gmail.com>
2020-04-16 18:54:24 +0530

    right table added

---
+++ IMS_DB/tblFormRightDetails.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblFormRightDetails]
+(
+	[FormID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FormName] NVARCHAR(50) NULL, 
+    [ParentID] INT NULL
+)
---
+++ IMS_DB/tblUserRights.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblUserRights]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserID] INT NULL, 
+    [FormID] INT NULL, 
+    [IsView] INT NULL, 
+    [IsSave] INT NULL, 
+    [IsUpdate] INT NULL, 
+    [IsDelete] INT NULL, 
+    [IsOther] INT NULL
+)
